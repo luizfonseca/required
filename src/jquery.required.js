1bb4f76dfa01dda3a334ee191ed01b29561b1a8b
Luiz Fonseca <runeroniek@gmail.com>
2011-09-26 17:06:51 -0400

    Initial release of required

---
+++ src/jquery.required.js
@@ -0,0 +1,28 @@
+(function(){
+	/*
+	* Required for jQuery
+	* Version: 1.0
+	* Usage:
+	*		jQuery('form.element').required()
+	*		jQuery('form.element').required({
+	*			message: "Oops, there's something wrong. Check it out.",
+	*			item_message: "Oops, you didn't fill this field"
+	*		})
+	*
+	*/
+	
+	$.fn.required = function(options){
+		
+		options = $.extend({
+			target: '#is_required_form'
+			message: 'All fields described below are required',
+			item_message: 'is required.',
+			callback: function(){}
+			
+		}, options);
+		
+		$(this).each(function(e){
+			alert(e);
+		});
+	}
+})(jQuery);
