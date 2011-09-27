45bce81bc52dd8dd4e283822ac2b1d6c2f0a9e72
Luiz Fonseca <runeroniek@gmail.com>
2011-09-26 21:57:25 -0400

    Generating errors, lack of style

--- src/jquery.required.js
+++ src/jquery.required.js
@@ -4,25 +4,60 @@
 * Usage:
 *  jQuery('form.element').required()
 *  jQuery('form.element').required({
-*     message: "Oops, there's something wrong. Check it out.",
-*    item_message: "Oops, you didn't fill this field"
+*     header: "Oops, there's something wrong. Check it out.",
+*     message: "Oops, you didn't fill this field."
 *  })
 *
 */
-(function(){
-	
-	$.fn.required = function(options){
+(function($){
+
+	$.fn.required = function(options)
+	{
+		var data = {}, error = [], error_list = '';
 		
-		options = $.extend({
-			target: '#is_required_form'
-			message: 'All fields described below are required',
-			item_message: 'is required.',
+		var options = $.extend(
+		{
+			header: 'All fields described below are required',
+			message: 'is required.',
 			callback: function(){}
 			
 		}, options);
 		
-		$(this).each(function(e){
-			alert(e);
+		$(this).submit(function(e)
+		{
+			e.preventDefault(); 
+			
+			$('input:not(:submit)', $(this)).each(function()
+			{
+				if ( $(this).hasClass('required') && $(this).val() === '')
+					error.push( $(this).siblings('label').attr('for', $(this).attr('name')).html() );
+			});
+			
+			if ( error.length > 0 ) 
+			{
+				error_list += "<h3> " + options.header + "</h3>";
+				error_list += "<ul>";
+			
+				for ( i = 0, l = error.length; i <= l; i++) 
+				{
+					if (error[i] != undefined || error[i] != null)
+					{
+						error_list += "<li><em> " + error[i] + "</em> " + options.message + " </li>" ;
+						//console.log(error[i]);
+					}
+				}
+				
+				error_list += "</ul>";
+			
+				$('#message_overflow').fadeIn();
+				$('#message_from_required').empty().html(error_list);
+				
+				
+				//console.log (error_list);
+			}
+			else options.callback();
+			$('#message_close_required').click(function(){ $('#message_overflow').fadeOut(); error_list = ''});
+			
 		});
 	}
 })(jQuery);
